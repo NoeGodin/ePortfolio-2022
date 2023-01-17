fb4cf91903f6d9bef16854a0bfbbabba770e9112
NoeGodin <noegodin2@gmail.com>
2023-01-17 21:37:54 +0100

    ajout animations

---
+++ js/animation.js
@@ -0,0 +1,19 @@
+const animation_elements = document.querySelectorAll('.animate-on-scroll, .animate-on-scroll2');
+
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add('animate');
+		} else {
+			entry.target.classList.remove('animate');
+		}
+	})
+}, {
+	threshold: 0.4
+});
+
+for (let i = 0; i < animation_elements.length; i++) {
+	const el = animation_elements[i];
+
+	observer.observe(el);
+}
